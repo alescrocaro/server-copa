ee9a36ad155be8e9212c798dcbd5a5fb4a3d4a67
alescrocaro <alescrocaro@gmail.com>
2022-11-21 21:38:35 -0300

    Add cors config

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import Fastify from 'fastify';
 import { PrismaClient } from '@prisma/client';
+import cors from '@fastify/cors';
 
 const prisma = new PrismaClient({
   log: ['query'], // log of all queries
@@ -9,6 +10,10 @@ async function bootstrap() {
   const fastify = Fastify({
     logger: true, // return logs 
   });
+
+  await fastify.register(cors, {
+    origin: true, // any application can access the server
+  })
 
   // http://localhost:3333/
 
@@ -20,7 +25,7 @@ async function bootstrap() {
     }
   });
 
-  await fastify.listen({ port: 3333 });
+  await fastify.listen({ port: 3333, host: '0.0.0.0' }); // host is for the mobile environment
 }
 
 bootstrap()
