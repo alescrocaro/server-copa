57a03eccf3e1edae2d10ed26cc596203d34442b9
alescrocaro <alescrocaro@gmail.com>
2022-11-21 21:23:18 -0300

    Add count pools query

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,9 @@
 import Fastify from 'fastify';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient({
+  log: ['query'], // log of all queries
+});
 
 async function bootstrap() {
   const fastify = Fastify({
@@ -7,9 +12,11 @@ async function bootstrap() {
 
   // http://localhost:3333/
 
-  fastify.get('/pools/count', () => {
+  fastify.get('/pools/count', async () => {
+    const count = await prisma.pool.count();
+
     return {
-      count: 7208
+      count
     }
   });
 
